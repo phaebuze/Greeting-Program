25a3517c49267e87d971a650650d7cd76a60eedf
Phaedra Buzek <124410661+phaebuze@users.noreply.github.com>
2024-10-30 16:01:04 -0400

    Update greetingProgramExample.py

--- greetingProgramExample.py
+++ greetingProgramExample.py
@@ -1,9 +1,4 @@
-def get_greeting(name):
-    # Partner 1: Complete this function to return a greeting message
-    pass
-
-def get_farewell(name):
-    # Partner 2: Complete this function to return a farewell message
-    pass
+import farewell
+import greeting
 
 # create main method to call functions
